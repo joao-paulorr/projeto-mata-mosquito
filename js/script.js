15855321fe0991a117f4299e5768c7f546176dc1
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-08-16 15:28:09 +0000

    d

--- js/script.js
+++ js/script.js
@@ -13,14 +13,18 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
-    var posicaoX = Math.floor(Math.random() * largura)
-    var posicaoY = Math.ceil(Math.random() * altura)
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.ceil(Math.random() * altura) - 90
 
     // Criar o elemento mosquito
     var mosquito = document.createElement('img')
-    mosquito.src = '../img.mosca.png'
+    mosquito.src = 'img/mosca.png'
     mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
 
+    document.body.appendChild(mosquito)
 
 }
 
