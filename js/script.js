8adeb11d8d7caa60908cac0d3e791ecae0e4b36d
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-08-16 17:38:24 +0000

    a

--- js/script.js
+++ js/script.js
@@ -16,10 +16,14 @@ function posicaoRandomica() {
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.ceil(Math.random() * altura) - 90
 
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+    // condicação ? expressão1 : expressão2
+
     // Criar o elemento mosquito
     var mosquito = document.createElement('img')
     mosquito.src = 'img/mosca.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
@@ -29,3 +33,16 @@ function posicaoRandomica() {
 }
 
 posicaoRandomica()
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
