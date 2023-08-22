286a1229869bd4d20296f5051278d2187ed14fdf
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-08-22 17:38:19 +0000

    a

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ var altura = 0
 var largura = 0
 var vidas = 1
 var tempo = 30
+var criaMosquitoTempo = 1500
+var nivel = window.location.search
+
+nivel = nivel.replace('?', '')
+
+if (nivel == 'normal'){
+    criaMosquitoTempo = 1500
+} else if (nivel == 'dificil'){
+    criaMosquitoTempo = 1000
+} else if(nivel == 'impossivel'){
+    criaMosquitoTempo == 500
+}
 
 function ajustaTamanhoPalcoJogo() {
 
